d7658852118a284dd2b47c7b60696e94d40e6001
paloma Schultze <palomaschultze@gmail.com>
2023-04-30 12:33:42 -0400

    Se redondearon los valores para que no afectaran los decimales al calculo y se agregaron los demás días de la semana

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
+function convert() {
+    let toggles = document.getElementById("seleccionarToggles");
+    let bigTemp = document.getElementById("bigTemp");
+    let items = document.querySelectorAll(".weakly-weather-item");
 
-function convert (temperatura){
-    let temperaturaCelsius = parseInt(temperatura.textContent);
-    let chequeada = true;
-    
-
-    if ($("#seleccionarToggles").is(":checked")) {
-        chequeada = false;
-    }
-
-    if (chequeada) {
-        let celsius = parseInt((temperaturaCelsius -32) / 1.8);
-        bigTemp.innerHTML = celsius;
-        $("#bigC").text("°C");
-        console.log(temperatura.textContent)
-        
-    }else{
-        let fahrenheit = parseInt(temperaturaCelsius * 1.8) + 32;
-        bigTemp.innerHTML = fahrenheit;
+    if (toggles.checked) {
+        // Cambiar a Fahrenheit
+        bigTemp.innerHTML = Math.round(parseFloat(bigTemp.innerHTML) * 9 / 5 + 32);
         $("#bigC").text("°F");
-
+        items.forEach(function (item) {
+            item.querySelector(".mb-0").innerHTML = Math.round(parseInt(item.querySelector(".mb-0").innerHTML) * 9 / 5 + 32);
+        });
+    } else {
+        // Cambiar a Celsius
+        bigTemp.innerHTML = Math.round((parseInt(bigTemp.innerHTML) - 32) * 5 / 9);
+        $("#bigC").text("°C");
+        items.forEach(function (item) {
+            item.querySelector(".mb-0").innerHTML = Math.round((parseInt(item.querySelector(".mb-0").innerHTML) - 32) * 5 / 9);
+        });
     }
-
 }
